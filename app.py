e03a072ce55930a5c7b86243f19104283e228d83
Charliewzyyy <3204305462@qq.com>
2023-11-11 15:19:37 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 import click
 
 from flask import Flask, render_template
-from flask import url_for
+from flask import request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
 ### 数据库配置
@@ -14,6 +14,8 @@ else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
+# 设置签名所需的密钥
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 # SQLAlchemy 数据库连接地址：
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
@@ -86,22 +88,72 @@ def inject_user():  # 函数名可以随意修改
     # 这个函数返回的变量（以字典键值对的形式）将会统一注入到每一个模板的上下文环境中，因此可以直接在模板中使用。
     # 后面我们创建的任意一个模板，都可以在模板中直接使用 user 变量。
 
+
 ### 404 错误处理函数
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
+### 编辑电影条目
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+# movie_id是电影条目记录在数据库中的主键值，用来在视图函数里查询对应的电影记录。
+def edit(movie_id):
+    # 返回对应主键的记录，如果没有找到，则返回 404 错误响应
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+            # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+
+### 删除电影条目
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
+
 
 ### 程序主页
 ### 在主页视图读取数据库记录
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])  # 同时接受GET和POST请求
 def index():
+    ### 创建电影条目
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的name值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('Item created.')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
     # user = User.query.first()  # 读取用户记录 # 有inject_user()了，就可以不用了
     movies = Movie.query.all()  # 读取所有电影记录
     # return render_template('index.html', user=user, movies=movies)
     return render_template('index.html', movies=movies)
-
 
 # ### 定义虚拟数据
 # name = 'Charliewzyyy'
