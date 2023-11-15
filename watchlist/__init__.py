6f67c3b726f8b5c65ef2cad5c42349a3694e28a6
Charliewzyyy <3204305462@qq.com>
2023-11-15 13:34:23 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -18,11 +18,11 @@ else:  # 否则使用四个斜线
 
 app = Flask(__name__)
 # 设置签名所需的密钥
-app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
-# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+# app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 # SQLAlchemy 数据库连接地址：
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
+# app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
 # 在扩展类实例化前加载配置
